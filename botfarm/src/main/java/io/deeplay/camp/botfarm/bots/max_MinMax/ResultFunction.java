74b1bd54ac46531dcc4b1d162b7195651b5c3cdf
Maksim Ruf <max.progg@yandex.ru>
2024-08-27 10:58:12 +0700

    feat - - переделка функции оценивания

--- botfarm/src/main/java/io/deeplay/camp/botfarm/bots/max_MinMax/ResultFunction.java
+++ botfarm/src/main/java/io/deeplay/camp/botfarm/bots/max_MinMax/ResultFunction.java
@@ -1,42 +1,101 @@
 package io.deeplay.camp.botfarm.bots.max_MinMax;
 
 import io.deeplay.camp.game.entities.Position;
+import io.deeplay.camp.game.entities.Unit;
 import io.deeplay.camp.game.mechanics.GameState;
 import io.deeplay.camp.game.mechanics.PlayerType;
 
 import java.util.List;
-import java.util.Random;
 
 public class ResultFunction implements UtilityFunction {
-    @Override
-    public double getUtility(GameState gameState, PlayerType playerType) {
-        double ownHp = 0;
-        double enemyHp = 0;
+  @Override
+  public double getUtility(GameState gameState, PlayerType playerType) {
+    double score = 0.0;
+      score = firstPlayerResult(gameState)-secondPlayerResult(gameState);
+    return score;
+  }
 
-        List<Position> ownUnitPositions;
-        List<Position> enemyUnitPositions;
+  private double firstPlayerResult(GameState gameState) {
+    double score = 0.0;
+    List<Position> playerUnitsPosition = gameState.getBoard().firstPlayerUnits();
+    score = playerUnitsPosition.size();
 
+    for (Position p : playerUnitsPosition) {
+      Unit unit = gameState.getBoard().getUnitByPosition(p);
 
-        if (playerType == PlayerType.FIRST_PLAYER) {
-            ownUnitPositions = gameState.getBoard().enumerateUnits(0, 2);
-            enemyUnitPositions = gameState.getBoard().enumerateUnits(2, 4);
-        } else {
-            ownUnitPositions = gameState.getBoard().enumerateUnits(2, 4);
-            enemyUnitPositions = gameState.getBoard().enumerateUnits(0, 2);
+      switch (unit.getUnitType()) {
+        case KNIGHT -> {
+          if (p.y() == 1) {
+            score++;
+          } else {
+            int hpFirstLineUnits = 0;
+            for (Position r : playerUnitsPosition) {
+              if (r.y() == 1) {
+                hpFirstLineUnits += gameState.getBoard().getUnitByPosition(r).getCurrentHp();
+              }
+            }
+            if (hpFirstLineUnits == 0) {
+              score++;
+            } else {
+              score -= 0.5;
+            }
+          }
         }
-
-        for (Position p : ownUnitPositions) {
-            ownHp += gameState.getBoard().getUnit(p.x(), p.y()).getCurrentHp();
+        case ARCHER, MAGE, HEALER -> {
+          if (p.y() == 0) {
+            score++;
+          } else {
+            score -= 0.5;
+          }
         }
-
-
-        for (Position p : enemyUnitPositions) {
-            enemyHp += gameState.getBoard().getUnit(p.x(), p.y()).getCurrentHp();
-        }
-
-        // Возвращаем разницу в здоровье своих юнитов и юнитов противника
-        // Чем больше разница, тем лучше для текущего игрока.
-        // Отрицательное значение — это плохо для текущего игрока.
-        return ownHp - enemyHp;
+        default -> throw new IllegalStateException("Unexpected value: " + unit.getUnitType());
+      }
     }
+    if (gameState.getArmyFirst().isBuffed) {
+      score *= 1.5;
+    }
+    return score;
+  }
+
+  private double secondPlayerResult(GameState gameState) {
+    double score = 0.0;
+    List<Position> playerUnitsPosition = gameState.getBoard().secondPlayerUnits();
+    score = playerUnitsPosition.size();
+
+    for (Position p : playerUnitsPosition) {
+      Unit unit = gameState.getBoard().getUnitByPosition(p);
+
+      switch (unit.getUnitType()) {
+        case KNIGHT -> {
+          if (p.y() == 2) {
+            score++;
+          } else {
+            int hpFirstLineUnits = 0;
+            for (Position r : playerUnitsPosition) {
+              if (r.y() == 2) {
+                hpFirstLineUnits += gameState.getBoard().getUnitByPosition(r).getCurrentHp();
+              }
+            }
+            if (hpFirstLineUnits == 0) {
+              score++;
+            } else {
+              score -= 0.5;
+            }
+          }
+        }
+        case ARCHER, MAGE, HEALER -> {
+          if (p.y() == 3) {
+            score++;
+          } else {
+            score -= 0.5;
+          }
+        }
+        default -> throw new IllegalStateException("Unexpected value: " + unit.getUnitType());
+      }
+    }
+    if (gameState.getArmySecond().isBuffed) {
+      score *= 1.5;
+    }
+    return score;
+  }
 }
